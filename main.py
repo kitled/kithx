1a008e326bd3bab3b5b264df5be42115abbf6ade
Chris L <pro@kit.gdn>
2024-06-19 09:29:53 +0200

    Create main.py. Wired SQLite 'data/logs.db'

---
+++ main.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from fasthtml.common import *
+from fasthtml.js import MarkdownJS, SortableJS
+
+
+db = database('data/logs.db')
+logs = db.t.logs
+if logs not in db.t:
+    logs.create(id=int, title=str, done=bool, name=str, details=str, priority=int, pk='id')
+Log = logs.dataclass()
+
