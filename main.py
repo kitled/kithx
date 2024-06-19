82f957b86efeb1189a9ee8c920190bc2905d9aaf
Chris L <pro@kit.gdn>
2024-06-19 09:33:40 +0200

    Updated /db path.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fasthtml.common import *
 from fasthtml.js import MarkdownJS, SortableJS
 
 
-db = database('data/logs.db')
+db = database('db/logs.db')
 logs = db.t.logs
 if logs not in db.t:
     logs.create(id=int, title=str, done=bool, name=str, details=str, priority=int, pk='id')
